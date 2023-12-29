d35c0c462454c0c6cc9caad1e6740d2d59a7335e
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-12-30 03:43:06 +0900

    Add symbol check on bybit mexc gateio

---
+++ crypto_listed_detector/fetchapi/gateio.py
@@ -0,0 +1,19 @@
+"""
+gateio api fetcher
+"""
+
+import requests
+
+
+class GateioFetch:
+    _BASE_URL = "https://api.gateio.ws"
+    def __init__(self):
+        pass
+
+    def get_contracts(self):
+        url = self._BASE_URL + "/api/v4/futures/usdt/contracts"
+        response = requests.get(url)
+        return response.json()
+
+    def get_all_linear_symbols(self):
+        return [item["name"] for item in self.get_contracts()]
---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+main.py
+"""
+import os
+import json
+
+
+# TODO: logging
+if __name__ == "__main__":
+    from crypto_listed_detector.detector import Detector
+
+    detector = Detector()
+
+    if not os.path.exists("symbols.json"):
+        detector.output_all_exchange_symbols()
+    else:
+        print("symbols.json already exists.")
+
+    all_symbols = json.load(open("symbols.json"))
+
+    # compare symbols
+    bybit_symbols = set(all_symbols["bybit"])
+    mexc_symbols = set(all_symbols["mexc"])
+    gateio_symbols = set(all_symbols["gateio"])
+
+    new_all_symbols = detector.get_all_exchange_symbols()
+
+    new_bybit_symbols = set(new_all_symbols["bybit"])
+    new_mexc_symbols = set(new_all_symbols["mexc"])
+    new_gateio_symbols = set(new_all_symbols["gateio"])
+
+    if bybit_symbols == new_bybit_symbols:
+        print("bybit symbols are the same")
+    else:
+        print("bybit symbols are different")
+        print("new symbols:")
+        print(new_bybit_symbols - bybit_symbols)
+
+    if mexc_symbols == new_mexc_symbols:
+        print("mexc symbols are the same")
+    else:
+        print("mexc symbols are different")
+        print("new symbols:")
+        print(new_mexc_symbols - mexc_symbols)
+
+    if gateio_symbols == new_gateio_symbols:
+        print("gateio symbols are the same")
+    else:
+        print("gateio symbols are different")
+        print("new symbols:")
+        print(new_gateio_symbols - gateio_symbols)
+
