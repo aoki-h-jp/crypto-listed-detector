61266ec7100933fad6f79d5885a657fc9e0d302d
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2024-01-02 16:19:51 +0900

    add bitget

---
+++ crypto_listed_detector/fetchapi/bitget.py
@@ -0,0 +1,21 @@
+"""
+bitget api fetcher
+"""
+
+import requests
+
+
+class BitgetFetch:
+    _BASE_URL = "https://api.bitget.com"
+
+    def __init__(self):
+        pass
+
+    def get_linear_ticker(self):
+        url = self._BASE_URL + "/api/v2/mix/market/tickers?productType=USDT-FUTURES"
+        response = requests.get(url)
+        print(response)
+        return response.json()
+
+    def get_all_linear_symbols(self):
+        return [item["symbol"] for item in self.get_linear_ticker()["data"]]
